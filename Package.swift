7ddeb255a292eb5be4e9134b74e54dec44e0e75a
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2022-09-20 20:02:55 -0300

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,24 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "RequestDL",
+    platforms: [.iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RequestDL",
-            targets: ["RequestDL"]),
+            targets: ["RequestDL"]
+        )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RequestDL",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "RequestDLTests",
-            dependencies: ["RequestDL"]),
+            dependencies: ["RequestDL"]
+        )
     ]
 )
