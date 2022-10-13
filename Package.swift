4321dbf284cdf2313cc970020903e1970370f076
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2022-10-13 10:26:35 -0300

    Removed docc script

--- Package.swift
+++ Package.swift
@@ -12,9 +12,7 @@ let package = Package(
             targets: ["RequestDL"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "RequestDL",
