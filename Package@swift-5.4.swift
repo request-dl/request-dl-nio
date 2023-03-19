1557ce51516d8bc316e8dcb8f58fa1c62906a0ac
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-03-19 07:44:05 -0300

    Added swift old versions support

---
+++ Package@swift-5.4.swift
@@ -0,0 +1,61 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RequestDL",
+    products: [
+//        .library(
+//            name: "RequestDL",
+//            targets: ["RequestDL"]
+//        ),
+        .library(
+            name: "RequestDLInternals",
+            targets: ["RequestDLInternals"]
+        )
+    ],
+    dependencies: [
+        /* DocC
+         .package(
+             url: "https://github.com/apple/swift-docc-plugin.git",
+             from: "1.0.0"
+         ),
+         DocC */
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
+
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.22.0"),
+        
+        .package(
+            url: "https://github.com/swift-server/async-http-client",
+            from: "1.15.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "RequestDLInternals",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client")
+            ]
+        ),
+
+        .testTarget(
+            name: "RequestDLInternalsTests",
+            dependencies: ["RequestDLInternals"]
+        )
+
+//        .target(
+//            name: "RequestDL",
+//            dependencies: ["RequestDLInternals"]
+//        ),
+//        .testTarget(
+//            name: "RequestDLTests",
+//            dependencies: ["RequestDL"],
+//            resources: [.process("Resources")]
+//        )
+    ]
+)
