91b77c8f4ec53f32ea2a296aaf2acb8d68d54c11
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-03-30 08:42:51 -0300

    Implemented all previous unit tests

---
+++ Tests/RequestDLTests/Task/DataTaskTests.swift
@@ -0,0 +1,40 @@
+/*
+ See LICENSE for this package's licensing information.
+*/
+
+import XCTest
+import RequestDLInternals
+@testable import RequestDL
+
+final class DataTaskTests: XCTestCase {
+
+    func testDataTask() async throws {
+        // Given
+        let certificate = RequestDLInternals.Certificates().server()
+        let output = "Hello World"
+
+        // When
+        try await InternalServer(
+            host: "localhost",
+            port: 8090,
+            response: output
+        ).run { baseURL in
+            let data = try await DataTask {
+                BaseURL(baseURL)
+                Path("index")
+
+                SecureConnection {
+                    Trusts(certificate.certificateURL.absolutePath())
+                }
+            }
+            .ignoresProgress()
+            .extractPayload()
+            .result()
+
+            let result = try HTTPResult<String>.resolve(data)
+
+            // Then
+            XCTAssertEqual(result.response, output)
+        }
+    }
+}
