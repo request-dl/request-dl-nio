ad942f1c8d0b5aea922ecf7135fdbef82ee5e1b7
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-03-31 08:04:36 -0300

    Fix unit tests

--- Tests/RequestDLTests/Certificates/SecureConnectionTests.swift
+++ Tests/RequestDLTests/Certificates/SecureConnectionTests.swift
@@ -9,7 +9,7 @@ final class SecureConnectionTests: XCTestCase {
 
     func testSecure_whenSetMaxTLSVersion_shouldBeValid() async throws {
         // Given
-        let maxVersion: RequestDL.TLSVersion = .v1_1
+        let maxVersion: RequestDL.TLSVersion = .v1_3
 
         // When
         let (session, _) = try await resolve(TestProperty {
@@ -42,7 +42,7 @@ final class SecureConnectionTests: XCTestCase {
     func testSecure_whenUpdatesTLSVersions_shouldBeValid() async throws {
         // Given
         let minVersion: RequestDL.TLSVersion = .v1
-        let maxVersion: RequestDL.TLSVersion = .v1_2
+        let maxVersion: RequestDL.TLSVersion = .v1_3
 
         // When
         let (session, _) = try await resolve(TestProperty {
