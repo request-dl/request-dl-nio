e50b8c668f834fbb9efaf3eec962e05364a3a6b1
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-04-25 19:10:07 -0300

    Added modifiers for changing behaviors of properties without the need of implementing a new Property

---
+++ Sources/RequestDL/Properties/Sources/Extra Properties/Modifier/Models/PropertyModifier.swift
@@ -0,0 +1,27 @@
+/*
+ See LICENSE for this package's licensing information.
+*/
+
+import Foundation
+
+/**
+ The `PropertyModifier` protocol defines a type that can modify a `Property` type.
+
+ This protocol requires the definition of an associated type `Body` that must be a `Property`
+ type, as well as a function `body(content:)` that takes in a `Content` parameter and returns
+ a `Body`.
+ */
+public protocol PropertyModifier {
+
+    typealias Content = _PropertyModifier_Content<Self>
+
+    associatedtype Body : Property
+
+    /**
+     Returns a modified `Property` type based on the given `Content`.
+
+     - Parameter content: The `Content` used to modify the `Property`.
+     - Returns: A modified `Property` type.
+     */
+    @PropertyBuilder func body(content: Self.Content) -> Self.Body
+}
