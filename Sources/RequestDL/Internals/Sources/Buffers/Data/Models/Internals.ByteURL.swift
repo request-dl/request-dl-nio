dafdc696bad93443669328f53ecdadf8bf6becbd
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-05-11 19:49:20 -0300

    Added ByteURL init from other ByteBuffer

--- Sources/RequestDL/Internals/Sources/Buffers/Data/Models/Internals.ByteURL.swift
+++ Sources/RequestDL/Internals/Sources/Buffers/Data/Models/Internals.ByteURL.swift
@@ -7,12 +7,28 @@ import NIOCore
 
 extension Internals {
 
-    class ByteURL {
+    final class ByteURL {
 
         lazy var buffer = NIOCore.ByteBuffer()
         var writtenBytes: Int = .zero
 
         init() {}
+
+        init(_ buffer: NIOCore.ByteBuffer) {
+            self.buffer = buffer
+            self.writtenBytes = buffer.writerIndex
+        }
+    }
+}
+
+extension Internals.ByteURL: Hashable {
+
+    static func == (_ lhs: Internals.ByteURL, _ rhs: Internals.ByteURL) -> Bool {
+        lhs === rhs
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
     }
 }
 
