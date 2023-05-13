ac869af1195030292b4599c3e2a27cc140eb0bb1
brenno <37243584+brennobemoura@users.noreply.github.com>
2023-05-13 11:15:36 -0300

    Removed deprecated code in some unit tests (#101)
    ## Description
    
    This PR removes deprecated methods that are use to tests other codes.

--- Tests/RequestDLTests/Properties/Sources/URL/Query Group/QueryGroupTests.swift
+++ Tests/RequestDLTests/Properties/Sources/URL/Query Group/QueryGroupTests.swift
@@ -37,7 +37,7 @@ class QueryGroupTests: XCTestCase {
         let property = QueryGroup {
             Query(123, forKey: "number")
             Query(1, forKey: "page")
-            Headers.Any("password", forKey: "api_key")
+            Headers.Any(name: "api_key", value: "password")
         }
 
         // When
