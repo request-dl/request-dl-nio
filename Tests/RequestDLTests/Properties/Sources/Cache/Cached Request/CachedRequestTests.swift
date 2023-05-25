32def4031f2f76ae568217d62e884ebe146f7cc3
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-05-25 08:35:52 -0300

    Changed time for waiting cache operations

--- Tests/RequestDLTests/Properties/Sources/Cache/Cached Request/CachedRequestTests.swift
+++ Tests/RequestDLTests/Properties/Sources/Cache/Cached Request/CachedRequestTests.swift
@@ -299,11 +299,11 @@ class CachedRequestTests: XCTestCase {
 extension CachedRequestTests {
 
     func waitCacheExpiration() async throws {
-        try await _Concurrency.Task.sleep(nanoseconds: 6_000_000_000)
+        try await _Concurrency.Task.sleep(nanoseconds: 5_000_000_000)
     }
 
     func waitCacheWriting() async throws {
-        try await _Concurrency.Task.sleep(nanoseconds: 1_000_000_000)
+        try await _Concurrency.Task.sleep(nanoseconds: 2_500_000_000)
     }
 
     func mockCachedData(_ headers: [(String, String)] = []) -> CachedData {
