12079fee843455b13333855413f9f5d2b2e0703d
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-05-29 08:27:36 -0300

    Add JSON verification before encoding

--- Sources/RequestDL/Properties/Sources/Payloads/Payload/Models/EncodingPayloadError.swift
+++ Sources/RequestDL/Properties/Sources/Payloads/Payload/Models/EncodingPayloadError.swift
@@ -4,4 +4,20 @@
 
 import Foundation
 
-public struct EncodingPayloadError: Error {}
+public struct EncodingPayloadError: Error {
+
+    public enum Context {
+        case invalidJSONObject
+        case invalidStringEncoding
+    }
+
+    // MARK: - Public properties
+
+    public let context: Context
+
+    // MARK: - Inits
+    
+    init(_ context: Context) {
+        self.context = context
+    }
+}
