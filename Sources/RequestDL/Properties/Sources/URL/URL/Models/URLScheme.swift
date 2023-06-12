296893c28e3db5ff5cd2ea216434829860444b5c
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-06-12 18:26:10 -0300

    renamed InternetProtocol to URLScheme

--- Sources/RequestDL/Properties/Sources/URL/URL/Models/URLScheme.swift
+++ Sources/RequestDL/Properties/Sources/URL/URL/Models/URLScheme.swift
@@ -5,57 +5,57 @@
 import Foundation
 
 /**
- The `InternetProtocol` struct is a data type that represents the protocol type used in a request
+ The `URLScheme` struct is a data type that represents the protocol type used in a request
 
  Usage:
 
  ```swift
- let internetProtocol: InternetProtocol = .https
+ let scheme: URLScheme = .https
  ```
 
  - Note: For a complete list of the available types, please see the corresponding static
  properties.
 
  - Important: If the internet protocol type is not included in the predefined static properties, use
- a string literal to initialize an instance of InternetProtocol.
+ a string literal to initialize an instance of URLScheme.
 
- The InternetProtocol struct conforms to the `ExpressibleByStringLiteral` protocol, allowing
+ The URLScheme struct conforms to the `ExpressibleByStringLiteral` protocol, allowing
  it to be initialized with a string literal, like so:
 
  ```swift
- let customInternetProtocol: InternetProtocol = "www"
+ let customScheme: URLScheme = "www"
  ```
  */
-public struct InternetProtocol: Sendable, Hashable {
+public struct URLScheme: Sendable, Hashable {
 
     // MARK: - Public static methods
 
     /// The HTTP protocol.
-    public static let http: InternetProtocol = "http"
+    public static let http: URLScheme = "http"
 
     /// The HTTPS protocol.
-    public static let https: InternetProtocol = "https"
+    public static let https: URLScheme = "https"
 
     /// The FTP protocol.
-    public static let ftp: InternetProtocol = "ftp"
+    public static let ftp: URLScheme = "ftp"
 
     /// The SMTP protocol.
-    public static let smtp: InternetProtocol = "smtp"
+    public static let smtp: URLScheme = "smtp"
 
     /// The IMAP protocol.
-    public static let imap: InternetProtocol = "imap"
+    public static let imap: URLScheme = "imap"
 
     /// The POP protocol.
-    public static let pop: InternetProtocol = "pop"
+    public static let pop: URLScheme = "pop"
 
     /// The DNS protocol.
-    public static let dns: InternetProtocol = "dns"
+    public static let dns: URLScheme = "dns"
 
     /// The SSH protocol.
-    public static let ssh: InternetProtocol = "ssh"
+    public static let ssh: URLScheme = "ssh"
 
     /// The Telnet protocol.
-    public static let telnet: InternetProtocol = "telnet"
+    public static let telnet: URLScheme = "telnet"
 
     // MARK: - Internal properties
 
@@ -75,15 +75,15 @@ public struct InternetProtocol: Sendable, Hashable {
 
 // MARK: - ExpressibleByStringLiteral
 
-extension InternetProtocol: ExpressibleByStringLiteral {
+extension URLScheme: ExpressibleByStringLiteral {
 
     /**
-     Initializes a `InternetProtocol` instance using a string literal.
+     Initializes a `URLScheme` instance using a string literal.
 
      - Parameter value: A string literal representing the internet protocol type.
-     - Returns: An instance of `InternetProtocol` with the specified internet protocol type.
+     - Returns: An instance of `URLScheme` with the specified internet protocol type.
 
-     - Note: Use this initializer to create a `InternetProtocol` instance from a string literal.
+     - Note: Use this initializer to create a `URLScheme` instance from a string literal.
      */
     public init(stringLiteral value: StringLiteralType) {
         self.init(value)
@@ -92,9 +92,14 @@ extension InternetProtocol: ExpressibleByStringLiteral {
 
 // MARK: - LosslessStringConvertible
 
-extension InternetProtocol: LosslessStringConvertible {
+extension URLScheme: LosslessStringConvertible {
 
     public var description: String {
         rawValue
     }
 }
+
+// MARK: - Deprecated
+
+@available(*, deprecated, renamed: "URLScheme")
+public typealias InternetProtocol = URLScheme
