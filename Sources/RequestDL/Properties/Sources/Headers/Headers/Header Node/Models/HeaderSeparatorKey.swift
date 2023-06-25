1953e77a9036dbdd072996c186441ab462a60e93
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2023-06-25 19:02:30 -0300

    swiftlint

--- Sources/RequestDL/Properties/Sources/Headers/Headers/Header Node/Models/HeaderSeparatorKey.swift
+++ Sources/RequestDL/Properties/Sources/Headers/Headers/Header Node/Models/HeaderSeparatorKey.swift
@@ -21,9 +21,13 @@ extension Property {
     /**
      Sets the header separator for the property.
 
-     This can be particularly useful if the server does not accept duplicated header keys. By setting the separator, the header values will be merged into a single value. This functionality is supported across all RequestDL operations.
+     This can be particularly useful if the server does not accept duplicated header keys. By setting the
+     separator, the header values will be merged into a single value. This functionality is supported across all
+     RequestDL operations.
 
-     The default behavior of SwiftNIO is to send and receive headers value by value. Since this behavior is well-suited for SwiftNIO, RequestDL provides the flexibility to change it to meet the specific requirements of the server specifications.
+     The default behavior of SwiftNIO is to send and receive headers value by value. Since this behavior is
+     well-suited for SwiftNIO, RequestDL provides the flexibility to change it to meet the specific requirements
+     of the server specifications.
 
      - Parameter headerSeparator: The header separator value. Defaults is `,`.
      - Returns: A modified version of the property with the updated header separator.
