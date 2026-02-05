65d8bd369a7fb66214d96cdf4df5ae2a4d7154c4
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2026-02-05 09:09:13 -0300

    Implemented tests

--- Tests/RequestDLTests/Properties/Sources/Secure Connection/SPKI Pinning/SPKIPinningTests.swift
+++ Tests/RequestDLTests/Properties/Sources/Secure Connection/SPKI Pinning/SPKIPinningTests.swift
@@ -2,21 +2,88 @@
  See LICENSE for this package's licensing information.
 */
 
+import Foundation
 import Testing
+import NIOSSL
+import Crypto
 @testable import RequestDL
 
 struct SPKIPinningTests {
 
     @Test
-    func pinning() async throws {
-        _ = try await DataTask {
-            BaseURL(.https, host: "www.google.com")
+    func pinning_whenCertificate_shouldBeValid() async throws {
+        // Given
+        let server = Certificates().server()
+        let client = Certificates().client()
+
+        let serverPin = try hashSPKI(from: server.certificateURL)
+        let clientPin = try hashSPKI(from: client.certificateURL)
+
+        // When
+        let resolved = try await resolve(TestProperty {
             SecureConnection {
                 SPKIPinning {
-                    SPKIHash("dJEGUdm2BuEalPybF+8enWB7R4AeiwE0gsQHyQrhzhY=")
+                    PropertyForEach(serverPin, id: \.self) {
+                        SPKIHash($0)
+                    }
+
+                    PropertyForEach(clientPin, id: \.self) {
+                        SPKIHash($0)
+                    }
                 }
             }
+        })
+
+        // Then
+        let secureConnection = try #require(resolved.session.configuration.secureConnection)
+        #expect(secureConnection.tlsPinningPolicy == .strict)
+        #expect(secureConnection.tlsPins == (serverPin + clientPin).map {
+            .init(source: .rawData($0), algorithm: SHA256.self)
+        })
+    }
+
+    @Test
+    func pinning_whenCertificateWithAuditPolicy_shouldBeValid() async throws {
+        // Given
+        let server = Certificates().server()
+        let client = Certificates().client()
+
+        let serverPin = try hashSPKI(from: server.certificateURL)
+        let clientPin = try hashSPKI(from: client.certificateURL)
+
+        // When
+        let resolved = try await resolve(TestProperty {
+            SecureConnection {
+                SPKIPinning(policy: .audit) {
+                    PropertyForEach(serverPin, id: \.self) {
+                        SPKIHash($0)
+                    }
+
+                    PropertyForEach(clientPin, id: \.self) {
+                        SPKIHash($0)
+                    }
+                }
+            }
+        })
+
+        // Then
+        let secureConnection = try #require(resolved.session.configuration.secureConnection)
+        #expect(secureConnection.tlsPinningPolicy == .audit)
+        #expect(secureConnection.tlsPins == (serverPin + clientPin).map {
+            .init(source: .rawData($0), algorithm: SHA256.self)
+        })
+    }
+}
+
+extension SPKIPinningTests {
+
+    func hashSPKI(from url: URL) throws -> [Data] {
+        try NIOSSLCertificate.fromPEMFile(
+            url.absolutePath(percentEncoded: false)
+        )
+        .map {
+            let bytes = try $0.extractPublicKey().toSPKIBytes()
+            return Data(SHA256.hash(data: Data(bytes)))
         }
-        .result()
     }
 }
