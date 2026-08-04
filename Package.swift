cc9f26c4477c917374b5296e552f94b75e820603
brennobemoura <37243584+brennobemoura@users.noreply.github.com>
2026-08-03 22:51:27 -0300

    Change packages

--- Package.swift
+++ Package.swift
@@ -18,65 +18,65 @@ let package = Package(
         )
     ],
     dependencies: [
-//        .package(
-//            url: "https://github.com/swift-server/async-http-client",
-//            from: "1.30.2"
-//        ),
-//        .package(
-//            url: "https://github.com/apple/swift-nio",
-//            from: "2.101.3"
-//        ),
+        .package(
+            url: "https://github.com/swift-server/async-http-client",
+            from: "1.30.2"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-nio",
+            from: "2.101.3"
+        ),
 //        .package(
 //            url: "https://github.com/apple/swift-nio-extras",
 //            from: "1.34.3"
 //        ),
-//        .package(
-//            url: "https://github.com/apple/swift-nio-ssl",
-//            from: "2.37.2"
-//        ),
+        .package(
+            url: "https://github.com/apple/swift-nio-ssl",
+            from: "2.37.2"
+        ),
         .package(
             url: "https://github.com/apple/swift-nio-transport-services",
             from: "1.26.0"
         ),
-//        .package(
-//            url: "https://github.com/apple/swift-log",
-//            from: "1.14.0"
-//        ),
-//        .package(
-//            url: "https://github.com/apple/swift-collections",
-//            from: "1.6.0"
-//        ),
-//        .package(
-//            url: "https://github.com/o-nnerb/swift-async-stream",
-//            from: "2.0.1"
-//        ),
-//        .package(
-//            url: "https://github.com/apple/swift-async-algorithms",
-//            from: "1.1.5"
-//        ),
-//        .package(
-//            url: "https://github.com/apple/swift-system",
-//            from: "1.6.3"
-//        ),
+        .package(
+            url: "https://github.com/apple/swift-log",
+            from: "1.14.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-collections",
+            from: "1.6.0"
+        ),
+        .package(
+            url: "https://github.com/o-nnerb/swift-async-stream",
+            from: "2.0.1"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-async-algorithms",
+            from: "1.1.5"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-system",
+            from: "1.6.3"
+        ),
     ],
     targets: [
         .target(
             name: "RequestDL",
             dependencies: [
-//                .product(name: "AsyncHTTPClient", package: "async-http-client"),
-//                .product(name: "SwiftAsyncStream", package: "swift-async-stream"),
-//                .product(name: "NIO", package: "swift-nio"),
-//                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                .product(name: "SwiftAsyncStream", package: "swift-async-stream"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
 //                .product(name: "NIOFoundationCompat", package: "swift-nio"),
-//                .product(name: "NIOHTTP1", package: "swift-nio"),
-//                .product(name: "NIOPosix", package: "swift-nio"),
-//                .product(name: "_NIOFileSystem", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "_NIOFileSystem", package: "swift-nio"),
 //                .product(name: "NIOHTTPCompression", package: "swift-nio-extras"),
-//                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
-//                .product(name: "SystemPackage", package: "swift-system"),
-//                .product(name: "Logging", package: "swift-log"),
-//                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "SystemPackage", package: "swift-system"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Collections", package: "swift-collections"),
             ]
         )
     ]
--- Sources/RequestDL/Export.swift
+++ Sources/RequestDL/Export.swift
@@ -3,20 +3,20 @@
 */
 
 
-//@_exported import AsyncHTTPClient
-//@_exported import SwiftAsyncStream
-//@_exported import NIOCore
-//@_exported import NIOConcurrencyHelpers
+@_exported import AsyncHTTPClient
+@_exported import SwiftAsyncStream
+@_exported import NIOCore
+@_exported import NIOConcurrencyHelpers
 //@_exported import NIOFoundationCompat
-//@_exported import NIOFileSystem
-//@_exported import NIOSSL
+@_exported import NIOFileSystem
+@_exported import NIOSSL
 @_exported import NIOTransportServices
-//@_exported import NIOHTTP1
-//@_exported import NIOPosix
+@_exported import NIOHTTP1
+@_exported import NIOPosix
 //@_exported import NIOHTTPCompression
-//@_exported import NIOFoundationEssentialsCompat
-//@_exported import SystemPackage
-//@_exported import Logging
-//@_exported import Collections
+@_exported import NIOFoundationEssentialsCompat
+@_exported import SystemPackage
+@_exported import Logging
+@_exported import Collections
 
 enum Internals {}
